78f5b23788cd4a6d1a2b6ce6637c4a60edd285ae
Juhyorim <joohr1234@gmail.com>
2023-06-21 04:35:15 +0900

    Initial commit

---
+++ src/main/java/dev/lime/SecurityTutorialApplication.java
@@ -0,0 +1,13 @@
+package dev.lime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecurityTutorialApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SecurityTutorialApplication.class, args);
+  }
+
+}
